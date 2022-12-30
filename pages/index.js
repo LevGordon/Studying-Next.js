3fb271526a1c31af270acf274c5d59bfb788349f
Lev_Gordon <lev.gordonsa@gmail.com>
2022-12-30 13:53:11 -0800

    url manipulation with router.push() and router.replace()

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home() {
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log("Order was placed")
+    router.push('/product')
+  }
+
+  // router.replace() can also be used when the desired url cannot be added to, but rather changed the already existing value. Acts similarly to replace attribute in Link. 
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -10,6 +21,9 @@ function Home() {
       <Link href="/product">
         <a>Products </a>
       </Link>
+      <button onClick={handleClick}>
+        Place Order
+      </button>
     </div>
  
   )
